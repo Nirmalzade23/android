dc805d2512bef9df52e54306b89206ee2e3af7d3
Nirmalzade23 <nirmalzade24@gmail.com>
2025-01-14 15:11:11 +0530

    insta colone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
     namespace = "com.example.instagramclone"
     compileSdk = 34
-
+buildFeatures {
+    viewBinding = true
+}
     defaultConfig {
         applicationId = "com.example.instagramclone"
         minSdk = 24
@@ -27,11 +30,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+
+    }
+    buildFeatures {
+        viewBinding = true
+
+
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
 }
 
@@ -42,8 +51,20 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+
+        implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+
+
+        implementation("com.google.firebase:firebase-analytics")
+
+
+        implementation("com.google.firebase:firebase-auth")
+        implementation("com.google.firebase:firebase-firestore")
+
 }
--- app/src/main/java/com/example/instagramclone/Signup.kt
+++ app/src/main/java/com/example/instagramclone/Signup.kt
@@ -6,15 +6,41 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class Signup : AppCompatActivity() {
+
+    val binding by com.example.instagramclone.databinding.ActivitySignupBinding.lazy {
+        ActivitySignupBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.singUpBtn.setOnClickListener {
+            if (binding.name.editText?.text.toString().equals("") or
+                binding.email.editText?.text.toString().equals("") or
+                binding.password.editText?.text.toString().equals("")
+            ) {
+                Toast.makeText(this@Signup, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+
+            } else {
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    binding.email.editText?.text.toString(),
+                    binding.password.editText?.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this@Signup, "Successfully Registered", Toast.LENGTH_SHORT)
+                            .show()
+                    }else{
+                        Toast.makeText(this@Signup, result.exception?.localizedMessage, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                }
+            }
         }
     }
 }
